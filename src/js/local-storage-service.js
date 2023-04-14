273e97fe4e42de34e98424168a055a23b701b44a
DianaShe <dikravchyk@gmail.com>
2023-04-14 16:45:59 +0300

    Realized add to watched/queue

--- src/js/local-storage-service.js
+++ src/js/local-storage-service.js
@@ -2,13 +2,7 @@ import { Notify } from "notiflix";
 
 const ERROR_MESSAGE = "Oops, something went wrong. Try again later."
 
-// У файлі рендеру модалки:
-// document.querySelector(".modal-movie__add-watched-btn").addEventListener('click', addFilmToLibrary(data, "watched"));
-// document.querySelector(".modal-movie__add-queue-btn").addEventListener('click', addFilmToLibrary(data, "queue"));
-
-
-
-function addFilmToLibrary(newFilm, key) {
+export function addFilmToLibrary(newFilm, key) {
     try {
         const savedData = JSON.parse(localStorage.getItem(key));
         
