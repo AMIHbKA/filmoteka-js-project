ba4409ed3f5ae6619bd4f99ec4d7a2be990745ce
Serhii Skliarov <114434801+Skaart77@users.noreply.github.com>
2023-04-16 22:25:18 +0300

    new

--- src/js/modalTeam.js
+++ src/js/modalTeam.js
@@ -11,7 +11,11 @@ const modal = basicLightbox.create(`
     <img src=${userUrl} alt="User" class="team-image">
     <p class="team-name">User</p>
     <p class="team-role">Team Lead</p>
-    <a href="https://github.com/user" target="_blank"></a>
+    <a class="social-list__link" href="https://github.com/user" target="_blank" rel="noopener noreferrer">
+     <svg class="icon-github" width="20" height="20">
+     <use href="./images/icons.svg#icon-github"></use>
+     </svg>
+     </a>
 </div>
 <div class="team-card">
     <img src=${userUrl} alt="User" class="team-image">
