82d2d18ecf71cf13141e5454387a26a626d26117
Ivan Klimov <klimov128@gmail.com>
2023-04-18 00:44:14 +0300

    add functions to customisation tui pagination
    Змінено першу та останню кнопки. Приближено максимально до макету.

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -5,13 +5,16 @@ import { renderDefaultMovies } from './renderDefaultMovies';
 
 let currentPage = 1;
 const galleryBox = document.querySelector('.movie__list');
+const screenWidth = window.innerWidth;
 
 export function startPagination() {
-  createDiv();
+  createContainer();
   fetchDefaultMovies(currentPage)
     .then(function (response) {
+      const totalItems = response.data.total_results;
       const totalPages = response.data.total_pages;
-      createPaginationContainer(totalPages);
+      createPaginationButtons(totalItems, totalPages);
+
       response.data.results;
       return;
     })
@@ -20,32 +23,44 @@ export function startPagination() {
     });
 }
 
-function createPaginationContainer(totalPages) {
-  const screenWidth = window.innerWidth;
+function createPaginationButtons(totalItems, totalPages) {
   const paginationOption = {
-    totalItems: totalPages,
+    totalItems: totalItems,
     itemsPerPage: 20,
     visiblePages: 9,
     page: currentPage,
     centerAlign: true,
+    template: {
+      moveButton: '<a href="#" class="tui-page-btn tui-{{type}}">' + '</a>',
+      disabledMoveButton:
+        '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' + '</span>',
+      moreButton:
+        '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+        '<span>...</span>' +
+        '</a>',
+    },
   };
-  console.log(screenWidth);
+
   const container = document.getElementById('tui-pagination-container');
   if (screenWidth < 768) {
-    paginationOption.visiblePages = 3;
+    paginationOption.visiblePages = 5;
   }
+
   const pagination = new Pagination(container, paginationOption);
+  customPaginationButtons(totalPages);
 
   pagination.on('afterMove', event => {
     currentPage = event.page;
-    fetchDefaultMovies(currentPage).then(r => {
+    scrollToTop();
+    fetchDefaultMovies(currentPage).then(response => {
+      customPaginationButtons(totalPages);
       clearList();
-      renderDefaultMovies(r.data.results);
+      renderDefaultMovies(response.data.results);
     });
   });
 }
 
-function createDiv() {
+function createContainer() {
   galleryBox.insertAdjacentHTML(
     'afterend',
     `<div id="tui-pagination-container" class="tui-pagination"></div>`
@@ -55,3 +70,59 @@ function createDiv() {
 function clearList() {
   galleryBox.innerHTML = '';
 }
+
+function customPaginationButtons(totalPages) {
+  rebuildLastButton(totalPages);
+  rebuildFirstButton();
+}
+
+function rebuildFirstButton() {
+  const firstButton = document.querySelector('.tui-first');
+  const firstChildButton = document.querySelector('.tui-first-child');
+  firstButton.textContent = 1;
+
+  if (Number(firstChildButton.innerText) === 1) {
+    firstButton.classList.add('hidden');
+    document.querySelector('.tui-prev').style.translate = '0';
+    return;
+  }
+
+  firstButton.classList.remove('hidden');
+  if (screenWidth < 768) {
+    document.querySelector('.tui-prev').style.translate = '0';
+    return;
+  }
+
+  document.querySelector('.tui-prev').style.translate = '-100%';
+  return;
+}
+
+function rebuildLastButton(totalPages) {
+  const lastButton = document.querySelector('.tui-last');
+  const lastChildButton = document.querySelector('.tui-last-child');
+  lastButton.textContent = totalPages;
+
+  if (Number(lastChildButton.innerText) === totalPages) {
+    lastButton.classList.add('hidden');
+    document.querySelector('.tui-next').style.translate = '0';
+    return;
+  }
+
+  lastButton.classList.remove('hidden');
+  if (screenWidth < 768) {
+    document.querySelector('.tui-next').style.translate = '0';
+    return;
+  }
+  document.querySelector('.tui-next').style.translate = '100%';
+  return;
+}
+
+const scrollToTop = () => {
+  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  if (scrollTop > 0) {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  }
+};
